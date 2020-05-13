36753eebeb10d219b5a71cbedaf8e31dfb33065e
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-13 16:44:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,17 @@
 class School
   attr_accessor :name, :roster
   
+    # Initialize with a name and an empty roster
   def initialize(name)
     @name = name
+    # Roster will have keys as grades and values as arrays of student names
     @roster = {}
   end
   
+    # Add student names to @roster by their grade 
   def add_student(name, grade)
-    !@roster[grade] ? @roster[grade] = [name] : @roster[grade] << name
+    @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
   end
+  
   
 end
